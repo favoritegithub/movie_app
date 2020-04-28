60035f50cf25abfe8d5006f2a7add85eab9b7127
favoritegithub <favoritemail.sooyoung@gmail.com>
2020-04-28 17:00:57 +0900

    Updates

---
+++ precache-manifest.45cd77ad81ac18fcb996b93c300a3a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc3d1b221594bcbc9491222ac8ea6a3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "767afef09865027d1093",
+    "url": "/movie_app/static/css/main.21ec163b.chunk.css"
+  },
+  {
+    "revision": "7056d556b146d289f55e",
+    "url": "/movie_app/static/js/2.4d727742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.4d727742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767afef09865027d1093",
+    "url": "/movie_app/static/js/main.0812ec2b.chunk.js"
+  },
+  {
+    "revision": "1bb1c6c9295ab4e1c1f2",
+    "url": "/movie_app/static/js/runtime-main.25c00a87.js"
+  }
+]);
